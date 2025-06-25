17ee5c05f7d12cd98b93a29cb7d889f71b7d2b2a
Wellington Venite <wellingtonvenite@gmail.com>
2025-06-24 22:11:54 -0300

    alterações nomes das pastas

---
+++ Jun 24.06/lista_vetores.py
@@ -0,0 +1,22 @@
+# numeros = [9, 18, 41, 65, 88, 121]
+
+# nomes = ["Rafael", "Ayslan", "Daniela", "Frank"]
+
+# num_float = [3290.90, 128.50, 85.90, 789.31]
+
+x = [0] * 5
+
+x[0] = int(input("Digite um número: "))
+x[1] = int(input("Digite um número: "))
+x[2] = int(input("Digite um número: "))
+x[3] = int(input("Digite um número: "))
+x[4] = int(input("Digite um número: "))
+
+print(x[0], x[1], x[2], x[3], x[4])
+
+print(x[4], x[3], x[2], x[1], x[0])
+
+print(x[0] * 2, x[1] * 2, x[2] * 2, x[3] * 2, x[4] * 2)
+
+print(x[0] / 2, x[1] / 2, x[2] / 2, x[3] / 2, x[4] / 2)
+
---
+++ Jun 24.06/vetores.py
@@ -0,0 +1,11 @@
+# nomes = ["Wellington", "Vitor", "Claudio"]
+
+# alunos = [""] * 30
+
+alunos_turma = int(input("Digite o total de alunos da turma: "))
+
+turma1 = [""] * alunos_turma
+
+turma1[0] = "Wellinton"
+
+print(f"Na posição, 0, temos o valor, {turma1[0]}")
