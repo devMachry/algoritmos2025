50386fdaaf4875b6b630b8663f60bbe6aa283feb
Wellington Machry Venite <108772073+devMachry@users.noreply.github.com>
2025-09-09 22:38:14 -0300

    Add files via upload

--- Terceiro trimestre/Set 09.09/aula.py
+++ Terceiro trimestre/Set 09.09/aula.py
@@ -5,12 +5,15 @@ for i in range(0, len(vetor)):
     vetor[i] = random.randint(0,100)
 
 print(f"Vetor aleatório: {vetor}")
-   
-for i in range(0, len(vetor)-1):
-    for cont in range(0, len(vetor)-1):
-        if(vetor[cont]>vetor[cont + 1]):
-            aux = vetor[cont]
-            vetor[cont]=vetor[cont+1]
-            vetor[cont + 1] = aux
 
-print(f"Vetor ordenado: {vetor}")
+def vetorOrdenado(vetor):
+    for i in range(0, len(vetor)-1):
+        for cont in range(0, len(vetor)-1):
+            if(vetor[cont]>vetor[cont + 1]):
+                aux = vetor[cont]
+                vetor[cont]=vetor[cont+1]
+                vetor[cont + 1] = aux
+    return vetor
+
+ordenado = vetorOrdenado(vetor)
+print(f"Vetor ordenado {ordenado}")
