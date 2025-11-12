5f5d77770724d19268feaa064b2de8a0cdb6692d
Wellington Machry Venite <108772073+devMachry@users.noreply.github.com>
2025-11-11 21:03:05 -0300

    Add files via upload

---
+++ aula11.11/sistema.py
@@ -0,0 +1,25 @@
+
+print("""
+        CADASTRO DE PRODUTOS
+        1. Cadastrar produto
+        2. Alterar produto
+        3. Excluir produto
+        4. Listar
+        5. Sair  
+    """)
+
+op = input("Digite uma opção: ")
+
+while(op != 5): 
+    if(op == 1):
+        pass
+    elif(op == 2):
+        pass
+    elif(op == 3):
+        pass
+    elif(op == 4):
+        pass
+    else:
+        break
+
+     
